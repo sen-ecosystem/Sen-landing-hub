5ce6d1645ce4e2791d5e5ae8485ee54684a16066
sen-ecosystem <ecosystemsen@gmail.com>
2026-08-19 16:58:04 +0200

    Update sw.js

--- sentew-app/sw.js
+++ sentew-app/sw.js
@@ -1,5 +1,5 @@
 // SEN TEW - Service Worker v10 (network-first)
-const CACHE = 'sentew-v36-2026-08-call-policy';
+const CACHE = 'sentew-v37-2026-08-clean';
 self.addEventListener('install', e => {
   self.skipWaiting();
 });
